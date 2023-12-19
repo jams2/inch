bed7f0987d8a19914eea130439efaaa8bdad8935
Joshua Munn <public@elysee-munn.family>
2023-12-19 10:00:01 +0000

    Add reader to compile function for env

--- src/Asm.hs
+++ src/Asm.hs
@@ -10,6 +10,7 @@ module Asm
     Immediate (..),
     mov,
     call,
+    add,
     ret,
     nop,
     (%),
@@ -54,6 +55,7 @@ data Instruction
   = Jmp !Operand
   | Mov !Operand !Operand
   | Call !Operand
+  | Add !Operand !Operand
   | Ret
   | Nop
   deriving (Show)
@@ -62,11 +64,15 @@ instance Emit Instruction where
   toText (Jmp x) = indent $ T.intercalate " " ["jmp", Emit.toText x]
   toText (Mov s d) = indent $ T.intercalate " " ["movq", Emit.toText s, ",", Emit.toText d]
   toText (Call x) = indent $ T.intercalate " " ["call", Emit.toText x]
+  toText (Add x y) = indent $ T.intercalate " " ["addq", Emit.toText x, Emit.toText y]
   toText Ret = indent "ret"
   toText Nop = indent "nop"
 
 mov :: (ToOperand a, ToOperand b) => a -> b -> Line
 mov x y = Instruction $ Mov (toOperand x) (toOperand y)
+
+add :: (ToOperand a, ToOperand b) => a -> b -> Line
+add x y = Instruction $ Add (toOperand x) (toOperand y)
 
 call :: T.Text -> Line
 call = Instruction . Call . Location
--- src/Compile.hs
+++ src/Compile.hs
@@ -13,14 +13,23 @@ where
 import AST
 import Arch
 import Asm
+import Control.Monad.Reader
+import Data.HashMap.Strict qualified as Map
 import Data.Text qualified as T
 
 newtype CompileError = CompileError T.Text deriving (Show)
 
 type CompilationResult = Either CompileError Program
 
+type Env a = Map.HashMap T.Text a
+
+type CompilationEnv = Reader (Env Primitive) CompilationResult
+
+-- | arity, compiler
+data Primitive = Primitive !Int !(Expr -> CompilationEnv)
+
 compileAll :: Expr -> CompilationResult
-compileAll p = (<> [ret]) . (compilePrologue <>) <$> compile p
+compileAll p = (<> [ret]) . (compilePrologue <>) <$> runReader (compile p) primitives
 
 compileFunctionHeader :: T.Text -> Program
 compileFunctionHeader name =
@@ -60,10 +69,19 @@ compilePrologue =
        ]
     <> compileFunctionHeader "L_scheme_entry"
 
-compile :: Expr -> CompilationResult
-compile NilExpr = Right [mov NilI RAX]
-compile TrueExpr = Right [mov TrueI RAX]
-compile FalseExpr = Right [mov FalseI RAX]
-compile (FixnumExpr i) = Right [mov (FixI i) RAX]
-compile (CharExpr c) = Right [mov (CharI c) RAX]
-compile _ = Right [nop]
+compile :: Expr -> CompilationEnv
+compile NilExpr = return $ Right [mov NilI RAX]
+compile TrueExpr = return $ Right [mov TrueI RAX]
+compile FalseExpr = return $ Right [mov FalseI RAX]
+compile (FixnumExpr i) = return $ Right [mov (FixI i) RAX]
+compile (CharExpr c) = return $ Right [mov (CharI c) RAX]
+compile _ = return $ Right [nop]
+
+compileFxadd1 :: Expr -> CompilationEnv
+compileFxadd1 (AppExpr (SymbolExpr "fxadd1") [rand]) = do
+  prologue <- compile rand
+  return ((<> [add (FixI 1) RAX]) <$> prologue)
+compileFxadd1 _ = return $ Left (CompileError "compileFxadd1: invalid invocation")
+
+primitives :: Env Primitive
+primitives = Map.fromList [("fxadd1", Primitive 1 compileFxadd1)]
